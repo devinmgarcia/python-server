12be6a46d8068b3d0860adfdd9048ea8c05ccfa7
Devin Garcia <devinmgarcia@gmail.com>
2021-07-16 15:15:35 -0500

    finished chap 13

--- request_handler.py
+++ request_handler.py
@@ -180,7 +180,6 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
 
     def do_PUT(self):
-        self._set_headers(204)
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
         post_body = json.loads(post_body)
@@ -188,22 +187,19 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Delete a single animal from the list
+        success = False
+
         if resource == "animals":
-            update_animal(id, post_body)
-            self.wfile.write("".encode())
+            success = update_animal(id, post_body)
+        # rest of the elif's
 
-        if resource == "employees":
-            update_employee(id, post_body)
-            self.wfile.write("".encode())
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
 
-        if resource == "customers":
-            update_customer(id, post_body)
-            self.wfile.write("".encode())
+        self.wfile.write("".encode())
 
-        if resource == "locations":
-            update_location(id, post_body)
-            self.wfile.write("".encode())
 
     def do_DELETE(self):
         # Set a 204 response code
